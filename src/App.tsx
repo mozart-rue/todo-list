8bab4d68cda0fa3d551557bf0b7aea59e0096efa
Mozart Rueckert <mozartrue@outlook.com>
2023-11-26 18:00:24 -0300

    feat: add input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-// import styles from "./app.module.css";
+import styles from "./app.module.css";
 import { Header } from "./components/Header";
+import { TextInput } from "./components/TextInput";
 
 function App() {
   return (
-    <div>
+    <div className={styles.input}>
       <Header />
+      <TextInput />
     </div>
   );
 }
