5c1f4db5c88b7597a76c60ba6337a160965cf108
Mozart Rueckert <mozartrue@outlook.com>
2023-11-26 14:38:10 -0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import styles from "./app.module.css";
+// import styles from "./app.module.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
