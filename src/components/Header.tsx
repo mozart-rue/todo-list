6e7c37cb85d89383505de3b0b17e022204b40963
Mozart Rueckert <mozartrue@outlook.com>
2023-11-26 14:51:12 -0300

    feat: add logo image to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 import styles from "./Header.module.css";
 
+import todoLogo from "../assets/todo-logo.svg";
+
 export function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <img alt="logo" src={todoLogo} />
+    </header>
+  );
 }
