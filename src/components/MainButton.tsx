fabe83afca00711ca783c3c7cface98c3c4c7aee
Mozart Rueckert <mozartrue@outlook.com>
2023-12-05 23:47:26 -0300

    feat: refactor -> set properties parameters

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,11 +1,14 @@
 import styles from "./MainButton.module.css";
-import { PlusCircle } from "phosphor-react";
 
-export function MainButton() {
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export function MainButton({ children, ...rest }: Props) {
   return (
-    <button className={styles.button}>
-      Criar
-      <PlusCircle />
+    <button className={styles.button} {...rest}>
+      {children}
     </button>
   );
 }
