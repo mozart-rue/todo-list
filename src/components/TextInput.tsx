48eee7eabfb15831c1b084460436050e89d85c41
Mozart Rueckert <mozartrue@outlook.com>
2023-12-05 23:52:43 -0300

    feat: add html input properties on parametres

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,11 +1,17 @@
 import styles from "./TextInput.module.css";
 
-export function TextInput() {
+export function TextInput({
+  ...rest
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>) {
   return (
     <input
       className={styles.input}
       type="text"
       placeholder="Adicione uma nova tarefa"
+      {...rest}
     />
   );
 }
