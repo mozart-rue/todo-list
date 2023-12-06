c492f5c6b0f8162de9e68bc514e4c3aa6a0ceeca
Mozart Rueckert <mozartrue@outlook.com>
2023-12-06 00:03:07 -0300

    feat: add functionality to add task in memory list

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header";
 import { MainButton } from "./components/MainButton";
 import { TextInput } from "./components/TextInput";
 import { ListHeader } from "./components/list/ListHeader";
+import { useState } from "react";
 
 export interface ITask {
   id: number;
@@ -12,13 +13,34 @@ export interface ITask {
 }
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleAddTask() {
+    if (!inputValue) {
+      return;
+    }
+
+    const newTask: ITask = {
+      id: new Date().getTime(),
+      text: inputValue,
+      isChecked: false,
+    };
+
+    setTasks((state) => [...state, newTask]);
+    setInputValue("");
+  }
+
   return (
     <main>
       <Header />
       <section className={styles.content}>
         <div className={styles.inputContainer}>
-          <TextInput />
-          <MainButton>
+          <TextInput
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+          <MainButton onClick={handleAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </MainButton>
